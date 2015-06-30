ae476cd84eb25cfc2cd2f0ca78446e6826cf0565
vitaly kononenko <konvita2@gmail.com>
2015-07-01 00:13:36 +0300

    first

---
+++ application/views/categ_edit.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="/css/bootstrap.css"/>
+        <link rel="stylesheet" href="/css/style.css"/>
+        <script type="text/javascript" src='/js/jquery-2.1.4.js'></script>
+        <script type="text/javascript" src="/js/bootstrap.js"></script>        
+    </head>   
+
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Сали</a>
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li><a href="#">Товары</a></li>
+                        <li><a href="#">Оплата и доставка</a></li>
+                        <li><a href="#">Контакты</a></li>                         
+                    </ul>
+                </div>
+            </div>
+        </nav>       
+    </body>
+    
+    
+    
+    
+</html>
